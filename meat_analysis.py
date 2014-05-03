a11679fa0eee34ec8975d2115731c7cfa35fa3e8
John Templon <jtemplon@gmail.com>
2014-05-03 17:12:10 -0400

    Added meta and water data to the data file

--- meat_analysis.py
+++ meat_analysis.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import csv
 
 data = {}
 data["payload"] = {"datasource":"faostat",
@@ -36,6 +37,7 @@ kg_to_animals = {"cow": 295, "pig": 70, "goat": 23, "chicken": 1.8, "fish": 0.8,
 
 countries = {}
 
+#This is where I meats and sum
 for c in food_data:
     country = c[2]
     meat_name = c[4]
@@ -53,10 +55,44 @@ for c in food_data:
         else:
             countries[country]["foods"][meat_type] = float(value)
 
+#This is where we add water usage
+water_usage = {"World":{}, "Brazil":{}, "China":{}, "Japan":{}, "United States of America":{}, 
+               "United Republic of Tanzania": {}}
+watercsv = open("Water_Footprints_Animals_mod.csv", "rU")
+for l in csv.reader(watercsv.readlines()[1:]):
+     animal = l[0]
+     water_usage["World"][animal] = float(l[1])
+     water_usage["Brazil"][animal] = float(l[2])
+     water_usage["China"][animal] = float(l[3])
+     water_usage["Japan"][animal] = float(l[4])
+     water_usage["United Republic of Tanzania"][animal] = float(l[5])
+     water_usage["United States of America"][animal] = float(l[6])
+#print water_usage
+for ck in countries.keys():
+    countries[ck]["water_footprint_per_capita"] = 0
+    for ak in countries[ck]["foods"].keys():
+        if ak == "fish":
+            pass
+        else:
+            countries[ck]["water_footprint_per_capita"] += int(countries[ck]["foods"][ak]*water_usage[ck][ak])
+watercsv.close()
+
+#Now we add in some other random data for countries to add context
+metacsv = open("fof_diet.csv", "rU")
+for l in csv.reader(metacsv.readlines()[1:]):
+    country = l[0]
+    countries[country]["kcal/day"] = l[1]
+    countries[country]["protein"] = l[2]
+    countries[country]["fat"] = l[3]
+    countries[country]["income"] = l[4]
+metacsv.close()
+
+#Add in the population and figure out the number of animals
 for ck in countries.keys():
     countries[ck]["population"] = pop_dict[ck]
     for ak in countries[ck]["foods"].keys():
         countries[ck]["foods"][ak] = int(countries[ck]["foods"][ak] * pop_dict[ck] / float(kg_to_animals[ak]))
+
 
 #This is to print data.
 # for ck in countries.keys():
